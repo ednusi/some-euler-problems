60338a6e3db5bbd756bd26ef7578c34d0d026761
Edward Nusinovich <edward.nusinovich@nist.gov>
2016-05-31 16:59:38 -0400

    Determine if finite expansion.

---
+++ 26.py
@@ -0,0 +1,21 @@
+# Any number that is (2^a)*(5^b) has a finite decimal approximation, and cycle of 1
+# Any number that is composite has a cycle as long as its prime factors??
+
+def fin_exp(number):
+
+	#this is true if the number is evenly divisble by 2
+	while (number/2.)==(number//2):
+		number = number//2
+
+	#the same for 5
+	while (number/5.)==(number//5):
+		number = number//5
+
+	if number==1:
+		return True
+    
+	else:
+		return False
+
+for num in xrange(1,10):
+	print num, fin_exp(num)
